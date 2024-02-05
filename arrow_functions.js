32d1fcc573acf175a1c344c7a9a7a07e129381fe
Clay O'Dell <clay.odell.music@gmail.com>
2024-02-05 17:25:57 -0600

    double and squareAndFindEvens turned into arrow functions

---
+++ arrow_functions.js
@@ -0,0 +1,6 @@
+const double = arr => arr.map(val => val * 2);
+
+console.log(double([1, 2, 3])); //just to demonstrate the code works
+
+const squareAndFindEvens = (numbers) => squares = numbers.map(val => val **2).filter(square => square % 2 === 0);
+console.log(squareAndFindEvens ([7, 12, 64])); //test code to see if it works.
